80d245a1c4bd66368acb08d33fb74e68efa4273b
hunor64 <nhunor06@gmail.com>
2024-11-22 09:21:57 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ColorPicker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void UpdateCanvas()
+        {
+            byte red = (byte)RedSlider.Value;
+            byte green = (byte)GreenSlider.Value;
+            byte blue = (byte)BlueSlider.Value;
+            ColorCanvas.Background = new SolidColorBrush(Color.FromRgb(red, green, blue));
+        }
+
+        private void RedSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            UpdateCanvas();
+        }
+
+        private void GreenSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            UpdateCanvas();
+        }
+
+        private void BlueSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            UpdateCanvas();
+        }
+
+    }
+}
